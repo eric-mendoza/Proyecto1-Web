49c4484602175959d6d9332b6bf49e9e7d0d0d87
eric-mendoza <eric.mendoza333@gmail.com>
2019-09-30 02:51:32 -0600

    Dejaron de funcionar imagenes :(

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,23 +8,25 @@ class Footer extends Component {
                     <div className="footer-top">
                         ¿Preguntas? Llama al +1 (408) 600-1719 (USA)
                     </div>
-                    <ul className="footer-links">
-                        <li>Preguntas frecuentes</li>
-                        <li>Centro de ayuda</li>
-                        <li>Cuenta</li>
-                        <li>Prensa</li>
-                        <li>Relaciones con inversionistas</li>
-                        <li>Empleo</li>
-                        <li>Formas de ver</li>
-                        <li>Términos de uso</li>
-                        <li>Privacidad</li>
-                        <li>Preferencias de cookies</li>
-                        <li>Información corporativa</li>
-                        <li>Contáctanos</li>
-                        <li>Prueba de velocidad</li>
-                        <li>Avisos legales</li>
-                        <li>Originales de Netflix</li>
-                    </ul>
+                    <div className="footer-links">
+                        <ul>
+                            <li>Preguntas frecuentes</li>
+                            <li>Centro de ayuda</li>
+                            <li>Cuenta</li>
+                            <li>Prensa</li>
+                            <li>Relaciones con inversionistas</li>
+                            <li>Empleo</li>
+                            <li>Formas de ver</li>
+                            <li>Términos de uso</li>
+                            <li>Privacidad</li>
+                            <li>Preferencias de cookies</li>
+                            <li>Información corporativa</li>
+                            <li>Contáctanos</li>
+                            <li>Prueba de velocidad</li>
+                            <li>Avisos legales</li>
+                            <li>Originales de Netflix</li>
+                        </ul>
+                    </div>
                     <div className="lang-switcher">
                         Español
                     </div>
